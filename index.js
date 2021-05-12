05fc54622a55952af383a9d88fd6c98ff1b13f8d
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-12 22:45:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   
-function convertTZ(date, tzString) {
-    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
-}
-
-
-const date = convertTZ(new Date(), "Asia/Kolkata") 
-
-
-
-
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-
- response.send('Current Date'+strTime);
+ response.send('Hi Pooja!');
 
 
    
